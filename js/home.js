055aab916eb1b6ce2f21597f03876345d95c0c5f
Brendan Pelto <peltobr1555@uwec.edu>
2022-02-17 15:03:46 -0600

    Added Sign in page with image and javascript for it

--- js/home.js
+++ js/home.js
@@ -1,7 +1,87 @@
-function login(showhide){
+/*function login(showhide){
   if(showhide == "show"){
       document.getElementById('popupbox').style.visibility="visible";
   }else if(showhide == "hide"){
       document.getElementById('popupbox').style.visibility="hidden"; 
   }
-  }
+  }*/
+    const model = document.getElementById('email-model');
+    const openBtn = document.querySelector('.sign-button');
+      const closeBtn = document.querySelector('.close-btn');
+      openBtn.addEventListener('click', () => {
+        model.style.display = 'block';
+      });
+      closeBtn.addEventListener("click", () => {
+        model.style.display = 'none';
+      });
+
+      window.addEventListener('click', (e) => {
+        if (e.target === model) {
+          model.style.display = 'none';
+        }
+      });
+
+      const form = document.getElementById('form');
+      const name = document.getElementById('name');
+      const email = document.getElementById('email');
+      const password = document.getElementById('password');
+      const passwordConfirm = document.getElementById('password-confirm');
+
+      function showError(input, message) {
+        const formValidation = input.parentElement;
+        formValidation.className = 'form-validation error';
+
+        const errorMessage = formValidation.querySelector('p');
+        errorMessage.innerText = message;
+      }
+
+      function showValid(input) {
+        const formValidation = input.parentElement;
+        formValidation.className = 'form-validation valid';
+      }
+
+      function checkRequired(inputArr) {
+        inputArr.forEach(function (input) {
+          if (input.value.trim() === '') {
+            showError(input, `${getFieldName(input)} is required`);
+          } else {
+            showValid(input);
+          }
+        });
+      }
+
+      function checkLength(input, min, max) {
+        if (input.value.length < min) {
+          showError(
+            input,
+            `${getFieldName(input)} must be at least ${min} characters`
+          );
+        } else if (input.value.length > max) {
+          showError(
+            input,
+            `${getFieldName(input)} must be less than ${max} characters`
+          );
+        } else {
+          showValid(input);
+        }
+      }
+
+      function checkPassMatch(input1, input2) {
+        if (input1.value !== input2.value) {
+          showError(input2, 'Passwords do not match');
+        }
+      }
+
+      function getFieldName(input) {
+        return input.name.charAt(0).toUpperCase() + input.name.slice(1);
+      }
+
+      form.addEventListener('submit', (e) => {
+        e.preventDefault();
+
+        checkRequired([name, email, password, passwordConfirm]);
+        checkLength(name, 3, 50);
+        checkLength(password, 8, 25);
+        checkLength(passwordConfirm, 8, 25);
+        passwordMatch(password, passwordConfirm);
+      });

de28aa1012ef6f08510d6aec21b28ea74d4e1401
Brendan Pelto <peltobr1555@uwec.edu>
2022-05-02 19:41:29 -0500

    changes to site

---
+++ js/notifications.js
@@ -0,0 +1,10 @@
+const accept1 = document.querySelector('notif-btn');
+const accept1Btn = document.getElementById('pay1');
+
+accept1.addEventListener('click', () => {
+    accept1Btn.style.display='none';
+})
+
+const accept1 = document.querySelector("pay1");
+const accept1 = document.querySelector("pay2");
+
